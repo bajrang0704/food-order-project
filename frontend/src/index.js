09be65454cfa5c06a1b858219cd0ecb34ffee583
bajrang0704 <prabanjanreddy0704004@gmail.com>
2025-06-16 13:13:41 +0530

    Update frontend config and index.js for Vercel deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
-import { transitions, positions, Provider as AlertProvider } from "react-alert";
+import{Provider} from "react-redux";
+import store from "./store"
+import{ transitions,positions,Provider as AlertProvider} from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
-const options = {
-  position: positions.BOTTOM_CENTER,
-  setTimeout: 5000,
-  offset: "30px",
-  transition: transitions.SCALE,
-};
 
-ReactDOM.render(
-  <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+const options ={
+  position : positions.BOTTOM_CENTER,
+  setTimeout:5000,
+  offset: "30px",
+  transition:transitions.SCALE,
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store ={store}>
+     <AlertProvider template={AlertTemplate} {...options}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
     </AlertProvider>
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
